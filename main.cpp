483dc3abe3f3a9a4edf899a5d166a9dc1bfa521d
P-Pix <guillaume200202@gmail.com>
2021-03-03 01:51:52 +0100

    image

--- main.cpp
+++ main.cpp
@@ -2,17 +2,35 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 using namespace std;
+using namespace sf;
+
+Texture open_image(const char NAME_IMAGE)
+{
+    Texture IMAGE_LOAD;
+    if(!IMAGE_LOAD.loadFromFile(NAME_IMAGE))
+    {
+        cout << "error" << endl;
+    }
+    return IMAGE_LOAD;
+}
+
+Sprite load_sprite(Texture IMAGE)
+{
+    Sprite SPRITE;
+    SPRITE.setTexture(IMAGE);
+    return SPRITE;
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1080, 720), "My window");
+    RenderWindow window(VideoMode(1080, 720), "My window");
     window.setFramerateLimit(60);
     while(window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while(window.pollEvent(event))
         {
-            if(event.type == sf::Event::Closed)
+            if(event.type == Event::Closed)
             {
                 window.close();
             }
